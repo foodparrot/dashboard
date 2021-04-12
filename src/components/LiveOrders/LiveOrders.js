03ba50b7e882ee67975bf5b1b1c5ecd924e7ab4c
Mohammad Azharuddin <azharuddinm2211@gmail.com>
2021-04-12 23:11:51 +0530

    updated

--- src/components/LiveOrders/LiveOrders.js
+++ src/components/LiveOrders/LiveOrders.js
@@ -4,6 +4,7 @@ function LiveOrders() {
     return (
         <div className="container">
             LiveOrders
+            <img style={{width:"1021px",height:"900px"}} src='images/mainlogo.svg'/>
         </div>
     )
 }
