b96f0d90ddcb1ac6c68a25f93d202e5364e65d34
Vikas cherala <69589147+vikas157@users.noreply.github.com>
2021-04-27 22:48:41 +0530

    view

--- src/components/View/View.js
+++ src/components/View/View.js
@@ -13,8 +13,8 @@ export class View extends Component {
                 <div className="flex-item item1"><b>Edit Menu</b></div>
             </div>
            {/*table*/}  
-               <div className="table-responsive-md">
-                   <table class="table table1 .table-borderless ">
+                <div className="table-responsive-md">
+                   <table class="table table1  ">
                       <tbody>
                           <tr>
                                 <td>
@@ -28,8 +28,8 @@ export class View extends Component {
                           </tr>
                       </tbody>
                    </table>
-               </div>
-               <div >
+                </div>
+               <div className="cunt" >
                     <button type="button" class="btn "><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                     </svg> Search Categories</button>
@@ -38,10 +38,10 @@ export class View extends Component {
                <div className="container cat ">
                     <span>Categories(2)</span>
                </div>
-               <div className="container cat">
+               <div className=" cat">
                     <div class="cart">
                         <table>
-                            
+
                         </table>
                     </div>
                </div>
