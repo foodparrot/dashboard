55e2e24f463bd1e26a10a5e64524bce07a2b7bde
sruthila <sruthila.vinnu@gmail.com>
2021-08-31 16:23:13 +0530

    updated

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,11 +3,6 @@ import Popup from 'reactjs-popup';
 import './product.css';
 
 function product() {
-  const [open, setOpen] = useState(false);
-
-  const eventModal = () => {
-    setOpen((o) => true);
-  }
     return (
                    <div className="col-12 col-md-5 box1">
                         <div className="row">
@@ -25,17 +20,30 @@ function product() {
                                         closeOnDocumentClick
                                         keepTooltipInside=".tooltipBoundary">
                                         <div className="menu menunew">
-                                           <div className="menu-item items itemsmenu"><button onClick={eventModal} type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-sm">Small modal</button></div>
+                                           <div className="menu-item items itemsmenu"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Launch demo modal</button></div>
+                                           <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog" role="document">
+                                             <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
                                            <div className="menu-item items itemsmenu"> Outlets</div>
                                            <div className="menu-item items itemsmenu"> Edit</div>
                                            <div className="menu-item items itemsmenu itemsmenu1"> DELETE</div>
-                                           <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-                                             <div class="modal-dialog modal-sm">
-                                             <div class="modal-content">
-                                                ...
-                                             </div>
-                                             </div>
-                                           </div>
                                         </div>
                                     </Popup>
                                     </div>
